51a5ceb8be6df0008256b49cc6a0e10b93125195
hariswarreddy <hariswarreddy241005@gmail.com>
2025-09-22 17:49:46 +0530

    changes for vercel error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,13 @@
-export { auth as middleware } from "@/auth";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+    
+  const token = req.cookies.get("token")?.value;
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
